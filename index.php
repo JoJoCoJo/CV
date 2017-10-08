6e0f6d33cae13f14207b31808db11ca07409c973
JoJoCoJo <jojocojo@msn.com>
2017-10-08 16:30:15 -0500

    Saving changes

--- index.php
+++ index.php
@@ -18,7 +18,27 @@
 	<script src="https://code.highcharts.com/modules/exporting.js"></script>
 	<script src="DataTables/js/jquery.dataTables.js"></script>
 </head>
-<body>
-	
+<body style="padding-top: 80px;">
+	<header>
+		<nav class="navbar fixed-top navbar-dark bg-dark">
+			<a class="navbar-brand" href="#" style="font-size: 30px;">JoJo CJ</a>
+		</nav>
+	</header>
+	<div class="row">
+		<div class="container">
+			<div class="jumbotron">
+			  <h1 class="display-3">José Joaquín Contreras Jorge</h1>
+			  <figure class="figure">
+			    <img src="..." class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+			    <figcaption class="figure-caption text-right">A caption for the above image.</figcaption>
+			  </figure>
+			  <hr class="my-4">
+			  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+			  <p class="lead">
+			    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+			  </p>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
